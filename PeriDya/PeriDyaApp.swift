6397b8fe2647758e459a153f6199fa78c3e3b46d
Endo <daniel.schmidt310383@gmail.com>
2026-01-24 16:26:04 +0100

    Initial Commit

---
+++ PeriDya/PeriDyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeriDyaApp.swift
+//  PeriDya
+//
+//  Created by Daniel Schmidt on 24.01.26.
+//
+
+import SwiftUI
+
+@main
+struct PeriDyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
